e1d0227e69a760cf673bda0a7eed354f6ef14056
Mark Hlawatschek <hlawatschek@atix.de>
2016-06-27 15:55:53 +0200

    Modified puppet.conf

--- puppet-config.sh
+++ puppet-config.sh
@@ -8,17 +8,16 @@ sudo yum -y install puppetserver
 
 cat <<EOF >> /etc/puppetlabs/puppet/puppet.conf
 [master]
-    certname = puppetmaster01.example.com
-    dns_alt_names = puppetmaster01,puppetmaster01.example.com,puppet,puppet.example.com
-    ca_name = 'Puppet CA generated on puppetmaster01.example.com at 2013-08-09 19:11:11 +0000'
-    reports = http,puppetdb
-    reporturl = https://localhost:443/reports/upload
-    node_terminus = exec
-    external_nodes = /etc/puppetlabs/puppet-dashboard/external_node
+    certname = puppetmaster.vagrant.local
+    ca_name = 'Puppet CA generated on puppetmaster.vagrant.local'
+    reports = files
+    #reporturl = https://localhost:443/reports/upload
+    #node_terminus = exec
+    #external_nodes = /etc/puppetlabs/puppet-dashboard/external_node
     ssl_client_header = SSL_CLIENT_S_DN
     ssl_client_verify_header = SSL_CLIENT_VERIFY
-    storeconfigs_backend = puppetdb
-    storeconfigs = true
+    #storeconfigs_backend = puppetdb
+    storeconfigs = false
     autosign = true
 EOF
 
